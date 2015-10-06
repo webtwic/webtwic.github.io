ec7132e2b64fd59fa1c386af38b9aace883b7191
Olaolu Olawuyi <whiskydee@gmail.com>
2015-10-06 07:12:04 -1200

    Release v1.02
    Added tooltips to article social. Fixed hamburger. Fixed mobile nav and
    fixed other issues

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@
 
  * Date Created: 20/08/2015
 
- * Last Modified: 27/09/2015
+ * Last Modified: 04/10/2015
 
  * Twitter: @whizkydee, @webtwic
 
@@ -122,7 +122,7 @@ jQuery(document).ready(function() {
    );
 
    $(".active-menu").append(
-      "<svg viewBox='0 0 32 32' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink'><g><path d='M 16.682,19.674c 0.010-0.012, 0.014-0.028, 0.024-0.040l 6.982-7.714c 0.39-0.434, 0.39-1.138,0-1.572 c-0.004-0.004-0.008-0.006-0.012-0.008C 23.496,10.13, 23.244,10, 22.964,10L 8.998,10 c-0.286,0-0.54,0.138-0.722,0.352L 8.272,10.348 c-0.39,0.434-0.39,1.138,0,1.572l 6.998,7.754C 15.66,20.108, 16.292,20.108, 16.682,19.674z'></path></g></svg>"
+      "<svg viewBox='0 0 32 32' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink'><path d='M 16.682,19.674c 0.010-0.012, 0.014-0.028, 0.024-0.040l 6.982-7.714c 0.39-0.434, 0.39-1.138,0-1.572 c-0.004-0.004-0.008-0.006-0.012-0.008C 23.496,10.13, 23.244,10, 22.964,10L 8.998,10 c-0.286,0-0.54,0.138-0.722,0.352L 8.272,10.348 c-0.39,0.434-0.39,1.138,0,1.572l 6.998,7.754C 15.66,20.108, 16.292,20.108, 16.682,19.674z'></path></svg>"
    );
 
 
@@ -142,10 +142,15 @@ jQuery(document).ready(function() {
    }
 
    $(window).scroll(function() {
-      ($(this).scrollTop() > height) ? $to_top.addClass("topVisible"): $to_top.removeClass("topVisible topFadeOut");
-      if ($(this).scrollTop() > 1200) {
-         $to_top.addClass("topFadeOut");
-      }
+      ($(this).scrollTop() > height) ? $to_top.addClass("topVisible"): $to_top.removeClass("topVisible");
+   });
+
+   // Styles for homepage article-social
+
+   $(".article .article-social a").hover(function() {
+      $("span", this).eq(1).fadeIn(400);
+   }, function() {
+      $("span", this).eq(1).fadeOut(250);
    });
 
    $to_top.on("click", function(e) {
