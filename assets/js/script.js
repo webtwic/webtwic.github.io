ebd1ae1d207f2009555ffd396c6ad8b1a43ac5a4
Olaolu Olawuyi <whiskydee@gmail.com>
2015-11-21 01:12:13 +0100

    Delete window.scroll

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,6 +13,9 @@
 
 */
 
+var s;
+s = $(window).scrollTop();
+
 jQuery(document).ready(function() {
 
 	// Define Variables
@@ -125,11 +128,12 @@ jQuery(document).ready(function() {
 	// Start Main Page
 
 	$("nav li ul.drop-nav").hide().removeClass("dropNavFallback");
-	$("nav li.dropdown").hover(
-		function() {
+
+	if ( $(window).width() > 860 ) {
+		$("nav li.dropdown").hover(function() {
 			$("ul.drop-nav", this).slideToggle("fast");
-		}
-	);
+		});
+	}
 
 	$(".active-menu").append(
 		"<svg viewBox='0 0 32 32' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink'><path d='M 16.682,19.674c 0.010-0.012, 0.014-0.028, 0.024-0.040l 6.982-7.714c 0.39-0.434, 0.39-1.138,0-1.572 c-0.004-0.004-0.008-0.006-0.012-0.008C 23.496,10.13, 23.244,10, 22.964,10L 8.998,10 c-0.286,0-0.54,0.138-0.722,0.352L 8.272,10.348 c-0.39,0.434-0.39,1.138,0,1.572l 6.998,7.754C 15.66,20.108, 16.292,20.108, 16.682,19.674z'></path></svg>"
@@ -182,21 +186,17 @@ jQuery(document).ready(function() {
 		$(".search-input input").removeClass("_is-focused");
 		$("#__expand").removeClass("visible");
 	});
+
 	$(".post-meta .post-meta-inner .cta-btns a.is_inactive").on("click", function(e) {
 		e.preventDefault();
 	});
+
 	$(document).keydown(function(e) {
     if (e.keyCode == 27) {
         closeUiOverlay();
     } else {
 		 return;
 	 }});
-
-	$(window).scroll(function() {
-		if ($(this).scrollTop() > 100) {
-			// alert("true");
-		}
-	});
 
 	$to_top.on("click", function(e) {
 		$("body,html").animate({
