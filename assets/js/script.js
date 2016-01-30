8f66008c2c547b01f864a5d7106b893da66144f4
Olaolu Olawuyi <whiskydee@gmail.com>
2016-01-30 16:27:15 +0100

    Move WBT and notif_text to top

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,6 +12,9 @@
 */
 var Webtwic = {} || Webtwic;
 
+var WBT = Webtwic,
+notif_text = "Some features which affect the functionality of webtwic are disabled in your browser. Please try refreshing the page or get a better <a style='color: #fff; border-bottom: 1px dotted #fff;' href='https://google.com/chrome' target='_blank'>browser.</a> <span class='close-button'></span>";
+
 Webtwic = {
 	// Basic Information about Webtwic
 	name: "Webtwic",
