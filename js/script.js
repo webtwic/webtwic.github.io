ec59f8f29a022a9a8978b7fd2403a3b417c6b46e
Olaolu Olawuyi <whiskydee@gmail.com>
2016-02-27 14:55:42 +0100

    Create $$ variable to return Webtwic object

--- js/script.js
+++ js/script.js
@@ -98,6 +98,7 @@ Webtwic = {
 	}
 
 }, WBT = Webtwic,
+$$ = Webtwic,
 notif_text = "Some features which affect the functionality of webtwic are disabled in your browser. Please try refreshing the page or get a better <a style='color: #fff; border-bottom: 1px dotted #fff;' href='https://google.com/chrome' target='_blank'>browser.</a> <span class='close-button'></span>";
 
 
